c71fa73baa198d1ebbbb554223fd738094cb0423
Greeshma_Alphaletz <greeshma.alphaletz@gmail.com>
2025-08-29 15:44:55 +0530

    Refactored code to handle input >=1000

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,33 +1,36 @@
 class StringCalculator {
-  int add(String numbers) {
-    if (numbers.isEmpty) return 0;
+  int add(String input) {
+    if (input.isEmpty) return 0;
 
-    String body = numbers;
+    String body = input;
     final delimiters = <String>[',', '\n'];
 
-    if (numbers.startsWith('//')) {
-      final delimiterEndIndex = numbers.indexOf('\n');
+    if (input.startsWith('//')) {
+      final delimiterEndIndex = input.indexOf('\n');
       if (delimiterEndIndex == -1) {
         throw ArgumentError(
           'Invalid input: custom delimiter line must end with \\n',
         );
       }
 
-      final delimiterPattern = numbers.substring(2, delimiterEndIndex);
+      final delimiterPattern = input.substring(2, delimiterEndIndex);
 
       if (delimiterPattern.isEmpty) {
         throw ArgumentError('Invalid input: empty custom delimiter');
       }
 
       delimiters.add(delimiterPattern);
-      body = numbers.substring(delimiterEndIndex + 1);
+      body = input.substring(delimiterEndIndex + 1);
     }
 
     final pattern = RegExp(delimiters.map(_escapeForRegex).join('|'));
-    final tokens = body.split(pattern);
+    final numbers = body
+        .split(pattern)
+        .where((s) => s.trim().isNotEmpty)
+        .map(int.parse)
+        .toList();
 
-    final nums = tokens.map(int.parse).toList();
-    final negatives = nums.where((n) => n < 0).toList();
+    final negatives = numbers.where((n) => n < 0).toList();
 
     if (negatives.isNotEmpty) {
       throw ArgumentError(
@@ -35,11 +38,7 @@ class StringCalculator {
       );
     }
 
-    int sum = tokens
-        .map((s) => s.trim())
-        .where((s) => s.isNotEmpty)
-        .map(int.parse)
-        .fold(0, (sum, n) => sum + n);
+    int sum = numbers.where((n) => n <= 1000).fold(0, (sum, n) => sum + n);
 
     return sum;
   }
