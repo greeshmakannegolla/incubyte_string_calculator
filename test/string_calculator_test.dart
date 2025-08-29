204e6f88b9c3e15ed8a42948d8dd60186f8b25b2
Greeshma_Alphaletz <greeshma.alphaletz@gmail.com>
2025-08-29 10:29:26 +0530

    Code refactor and extra spaces, commas handling test case added

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -2,23 +2,30 @@ import 'package:string_calculator/string_calculator.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Should return 0 for empty string', () {
+  group('StringCalculator - basics', () {
     final stringCalculator = StringCalculator();
-    expect(stringCalculator.add(""), 0);
+    test('Should return 0 for empty string', () {
+      expect(stringCalculator.add(""), 0);
+    });
+
+    test('Should return number itself for single number', () {
+      expect(stringCalculator.add("5"), equals(5));
+    });
+
+    test('Should sum two comma-separated numbers', () {
+      expect(stringCalculator.add("1,5"), equals(6));
+    });
   });
 
-  test('Should return number itself for single number', () {
+  group('Any amount of numbers', () {
     final stringCalculator = StringCalculator();
-    expect(stringCalculator.add("5"), equals(5));
-  });
 
-  test('Should sum two comma-separated numbers', () {
-    final stringCalculator = StringCalculator();
-    expect(stringCalculator.add("1,5"), equals(6));
-  });
+    test('Should handle multiple numbers', () {
+      expect(stringCalculator.add("1,2,3,4"), equals(10));
+    });
 
-  test('Should handle multiple numbers', () {
-    final stringCalculator = StringCalculator();
-    expect(stringCalculator.add("1,2,3,4"), equals(10));
+    test('Should handle spaces and extra commas gracefully', () {
+      expect(stringCalculator.add(' 1, 2 , , 3 '), 6);
+    });
   });
 }
